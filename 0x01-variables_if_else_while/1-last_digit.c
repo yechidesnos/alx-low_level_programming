cff417eb9e2048e21297a9d25a405200aafb436f
yechidesnos <edyechi49@gmail.com>
2022-04-10 06:39:25 +0200

    use for and putchar

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,12 +7,12 @@
 int main(void)
 {
 	int n;
-    int r;
+int r;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-    r = n % 10;
+r = n % 10;
 
 	if (r > 5)
 		printf("Last digit of %d is %d and is greater than 5\n",
