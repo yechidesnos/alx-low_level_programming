b89484cc88157ea1dcb1be00f50addc5976c12e5
yechidesnos <edyechi49@gmail.com>
2022-04-10 06:17:42 +0200

    use for and putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(){
+    char alph;
+    for (alph = 'a' ; alph <= 'z' ; alph++)
+    {putchar (alph);
+    putchar ("\n");
+    }
+    return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(){
+    char alph;
+    for (alph = 'a' ; alph <= 'z' ; alph++)
+    {putchar (alph);
+   
+    }
+    for (alph = 'A' ; alph <= 'Z' ; alph++)
+    {putchar (alph);
+    
+    }putchar ('\n');
+    return (0);
+}
