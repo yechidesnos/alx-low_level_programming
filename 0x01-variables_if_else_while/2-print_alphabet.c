4bc38c8f543ca03fb91a1b7ad09f75bdd8f79ae0
yechidesnos <edyechi49@gmail.com>
2022-04-12 13:51:12 +0200

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-int main(void){
+/**
+*main - main block
+*Description:	print the character.
+*Return: (0) that's great.
+*/
+int main(void)
+{
     char alph;
+    
     for (alph = 'a' ; alph <= 'z' ; alph++)
-    {putchar (alph);
+    {putchar (alph); }
     putchar ('\n');
-    }
     return (0);
-}
+    }
+    
