99d480a97b40a6419d666377cffff528805c4522
yechidesnos <edyechi49@gmail.com>
2022-04-12 13:36:06 +0200

    show the last digit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-int main(void){
+/**
+*main - main block
+*Description:	print the character.
+*Return: (0) that's great.
+*/
+int main(void)
+{
     char alph;
     for (alph = 'a' ; alph <= 'z' ; alph++)
-    {putchar (alph);
-    }
+    {putchar (alph);}
     for (alph = 'A' ; alph <= 'Z' ; alph++)
-    {putchar (alph);
-    }putchar ('\n');
+    {putchar (alph);}
+        putchar ('\n');
     return (0);
 }
