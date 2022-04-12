36ecb2e8ac86bc3290c2fb468530bf4491f41919
yechidesnos <edyechi49@gmail.com>
2022-04-12 13:54:20 +0200

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,11 @@
 */
 int main(void)
 {
-    char alph;
-    
-    for (alph = 'a' ; alph <= 'z' ; alph++)
-    {putchar (alph); }
-    putchar ('\n');
-    return (0);
-    }
-    
+	char alph;
+	
+	for (alph = 'a' ; alph <= 'z' ; alph++)
+	{putchar (alph); }
+	putchar ('\n');
+	return (0);
+}
+
