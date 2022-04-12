560d33d96fa847beffaa4246d439a905e629be7b
yechidesnos <edyechi49@gmail.com>
2022-04-12 13:46:17 +0200

    putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,11 @@
 int main(void)
 {
 	char alph;
+
 	for (alph = 'a' ; alph <= 'z' ; alph++)
-		{putchar (alph);}
+		{putchar (alph); }
 	for (alph = 'A' ; alph <= 'Z' ; alph++)
-		{putchar (alph);}
+		{putchar (alph); }
 		putchar ('\n');
 	return (0);
 }
