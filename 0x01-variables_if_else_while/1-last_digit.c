1f3021290eeb32d0185561926168cd41b3154585
yechidesnos <edyechi49@gmail.com>
2022-04-12 13:25:18 +0200

    use for and putchar

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,6 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
+/**
+*main - main block
+*Description:	give the last digit.
+*Return: (0) that's great.
+*/
 
 /* betty style doc for function main goes there */
 int main(void)
@@ -20,7 +25,6 @@ r = n % 10;
 	else if (r == 0)
 		printf("Last digit of %d is %d and is 0\n", n, r);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-		       n, r);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, r);
 	return (0);
-}
+	}
