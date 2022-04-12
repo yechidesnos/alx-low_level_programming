6a8a284e840dbf1a90cbcd826fd1714adec13e04
yechidesnos <edyechi49@gmail.com>
2022-04-12 14:30:07 +0200

    print the character. except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,8 +11,10 @@ int main(void)
 	char alph;
 
 	for (alph = 'a' ; alph <= 'z' ; alph++)
-		{ if (alph != 'e' && alph != 'q')
-            putchar (alph); }
-		putchar ('\n');
+		{ 
+            if (alph != 'e' && alph != 'q')
+            putchar (alph); 
+        }
+        putchar ('\n');
 	return (0);
 }
