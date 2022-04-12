6d9679846d354e9afc6b027e71d485c900f929ec
yechidesnos <edyechi49@gmail.com>
2022-04-12 13:38:48 +0200

    putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,12 @@
 *Return: (0) that's great.
 */
 int main(void)
-{
+{   
     char alph;
     for (alph = 'a' ; alph <= 'z' ; alph++)
     {putchar (alph);}
     for (alph = 'A' ; alph <= 'Z' ; alph++)
     {putchar (alph);}
-        putchar ('\n');
+    putchar ('\n');
     return (0);
 }
