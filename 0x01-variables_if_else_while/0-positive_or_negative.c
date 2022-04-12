725e36cd3469c6a44a174dc6e84182eb960aec20
yechidesnos <edyechi49@gmail.com>
2022-04-12 12:44:06 +0200

    use for and putchar

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,21 +1,25 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
-int main(void){
-	
+/* 
+main - main block
+Description: 	inform that the random number is positive or negative
+*/
+int main(void)
+{
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	
 	if (n > 0)
-		printf("%d is positive\n", n);
+		    printf("%d is positive\n", n);
 	else if (n < 0)
-	       printf("%d is negative\n", n);
+	        printf("%d is negative\n", n);
 	else 
-		printf("%d is zero\n", n);
+		    printf("%d is zero\n", n);
+
 	return (0);	
 }
 
