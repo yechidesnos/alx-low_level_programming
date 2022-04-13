1d7b1a8bc1f3acff8fb40507a82e72d46c914355
yechidesnos <edyechi49@gmail.com>
2022-04-13 23:09:41 +0200

    check if the letter is lowercase or upercase and putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,16 +4,18 @@
 /**
 *main - main block
 *print_alphabet
-*Description:	print_alphabet.
+@c: type void character
+*Description:print_alphabet.
 *Return: (0) that's great.
 */
 void print_alphabet(void)
 {
-    char alph;
-    
-    for (alph = 'a'; alph <= 'z'; alph++)
-    {
-        putchar(alph);
-    }
-    putchar('\n');
+	char alph;
+	
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
 }
+
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,22 +2,24 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main - main block2-
+*main - main block2
+*@c: type void character
 *print_alphabet_x10.c
 *Description:	print_alphabet_x10.
 *Return: (0) that's great.
 */
 void print_alphabet_x10(void)
 {
-    char alph;
-    int i;
-
-    for (i = 0; i <= 9; i++)
-    {
-        for (alph = 'a'; alph <= 'z'; alph++)
-        {
-            putchar(alph);
-        }
-        putchar('\n');
-    }
+	char alph;
+	int i;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			putchar(alph);
+		}
+		putchar('\n');
+	}
 }
+
