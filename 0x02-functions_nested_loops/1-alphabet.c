f4cd7c9a3f30893134317fa7ec22e19185e40ef8
yechidesnos <edyechi49@gmail.com>
2022-04-14 01:35:08 +0200

    check if the letter is lowercase or upercase and putchar add number

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,6 +8,7 @@
 void print_alphabet(void)
 {
 	char alph;
+
 	for (alph = 'a'; alph <= 'z'; alph++)
 	{
 		putchar(alph);
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * _abs - function that computes the absolute value of an integer.
+  * @n: takes a int
+  * Return: the abs
+  */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
