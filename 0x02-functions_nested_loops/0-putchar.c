17c5f5f26f0090117796c7859305d89b94febeb7
yechidesnos <edyechi49@gmail.com>
2022-04-13 03:10:06 +0200

    print the character and the digit with putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - main block
+*Description:	print single digit from 0 to 10.
+*Return: (0) that's great.
+*/
+int main(void)
+{
+    printf ("_putchar"); 
+    putchar('\n') ;
+    return (0); 
+}
