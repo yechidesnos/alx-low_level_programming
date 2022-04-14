5b64a43a04ddfdab0bdc02c31e49a58ebb8f39e0
yechidesnos <edyechi49@gmail.com>
2022-04-14 02:43:40 +0200

    table and fibonacci fonction

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,7 +4,7 @@
 
 /**
  * main - main block
- * Description: prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.
+ * Description: prints the sum of all the multiples of 3 or 5 <1024
  * Return: 0
  */
 int main(void)
