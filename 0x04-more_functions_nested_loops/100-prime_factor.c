bb72299d44a0ed99c09f70aaa1e89c8167841a8e
yechidesnos <edyechi49@gmail.com>
2022-07-09 00:08:55 +0200

    advance exercice

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+/**
+ * main - Affiche
+ *
+ * Return: toujours 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_number - une fonction qui affiche un entier
+ * @n: entree
+ * Return: le nombre entré
+ */
+void print_number(int n)
+{
+
+	return (0);
+}
