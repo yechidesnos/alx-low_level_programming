5d933749d77d5ae6d4559bcb22f2f54d37e71464
yechidesnos <edyechi49@gmail.com>
2022-07-08 18:31:54 +0200

    is digit scrypt

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "desnos.h"
+
+/**
+ * mul -est une fonction qui fait le produit de deux nombres.
+ * @a: premier argument.
+ * @b: deuxieme argument.
+ * return: retourne le produit des deux nombres passé en argument.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
