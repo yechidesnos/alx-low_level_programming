ecbc4211af1e37d9945fce38f90bb1937c3dc9a5
yechidesnos <edyechi49@gmail.com>
2022-07-12 11:28:41 +0200

    alx pointer exercice

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * swap_int - the function who permute two integer
+ * @a - first parameter
+ * @b - second parameter
+ * return: the permute number
+ * */
+void swap_int(int *a, int *b);
+{
+	*a = b;
+	*b = a;
+}
